6c5464790ccb52c1e12fd29d1eca84548dd0d8aa
Chetan Solanki <72223599+schets14@users.noreply.github.com>
2025-01-23 16:21:40 +0530

    Update variables.tf

--- aws-vpc/variables.tf
+++ aws-vpc/variables.tf
@@ -2,7 +2,7 @@ variable "eks_vpc_cidr" {
     default = "10.0.0.0/16"
 }
 variable "public_sub1_cidr" {
-    default = "10.0.0.0/24"
+    default = "10.0.2.0/24"
 }
 variable "public_sub2_cidr" {
     default = "10.0.1.0/24"
